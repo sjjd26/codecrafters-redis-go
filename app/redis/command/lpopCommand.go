6a8b459546461108727d4e679d8dbaa376475755
Stuart Dingwell <stuartdingwell@gmail.com>
2025-08-13 22:27:19 +0100

    support lpop count argument

--- app/redis/command/lpopCommand.go
+++ app/redis/command/lpopCommand.go
@@ -1,38 +1,64 @@
 package command
 
 import (
+	"fmt"
+	"strconv"
+
 	cmderrors "github.com/codecrafters-io/redis-starter-go/app/redis/command/cmdErrors"
 	"github.com/codecrafters-io/redis-starter-go/app/redis/store"
 	"github.com/codecrafters-io/redis-starter-go/app/redis/types"
 )
 
 type LPopCommand struct {
-	Key   string
-	Store *store.RedisStore
+	Key           string
+	CountProvided bool
+	Count         int
+	Store         *store.RedisStore
 }
 
 func NewLPopCommand(args []string, ctx *CommandContext) (Command, error) {
 	if len(args) < 1 {
 		return nil, cmderrors.ErrNotEnoughArgs
-	} else if len(args) > 1 {
+	} else if len(args) > 2 {
 		return nil, cmderrors.ErrTooManyArgs
 	}
 
 	key := args[0]
+	count := 1
+	countProvided := false
+	if len(args) == 2 {
+		var err error
+		count, err = strconv.Atoi(args[1])
+		if err != nil {
+			return nil, fmt.Errorf("invalid count argument: %v", err)
+		}
+		countProvided = true
+	}
+
 	return &LPopCommand{
-		Key:   key,
-		Store: ctx.Store,
+		Key:           key,
+		Count:         count,
+		CountProvided: countProvided,
+		Store:         ctx.Store,
 	}, nil
 }
 
 func (cmd *LPopCommand) Execute() (string, error) {
 	list, exists := cmd.Store.GetList(cmd.Key)
-	if !exists {
+	if !exists || len(list) == 0 {
 		return types.BulkStringNull, nil
 	}
-	value := list[0]
-	list = list[1:]
+
+	if cmd.Count > len(list) {
+		cmd.Count = len(list)
+	}
+	values := list[:cmd.Count]
+	list = list[cmd.Count:]
 	listValue := store.NewRedisList(list)
 	cmd.Store.Add(cmd.Key, listValue)
-	return types.CreateBulkString(value), nil
+
+	if cmd.CountProvided {
+		return types.CreateBulkStringArray(values), nil
+	}
+	return types.CreateBulkString(values[0]), nil
 }
